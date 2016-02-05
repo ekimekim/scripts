e58e7d94c384fe2d7a386efbbf4cc5d8e95ab28f
Mike Lang <michael.lang@mixpanel.com>
2016-02-05 19:31:58 +0000

    fab-complete: Also complete roles

--- fab-completion.bash
+++ fab-completion.bash
@@ -1,7 +1,13 @@
 
 _fab_completion() {
     local current=${COMP_WORDS[COMP_CWORD]}
-    COMPREPLY=( $(compgen -W "$(run-fab --shortlist)" -- "$current") )
+    local prev=${COMP_WORDS[COMP_CWORD-1]}
+	if egrep -qe '-[^=]*R' <<<"$prev"; then
+		options="$(run-fab print_roles)"
+	else
+		options="$(run-fab --shortlist)"
+	fi
+    COMPREPLY=( $(compgen -W "$options" -- "$current") )
 }
 
 complete -F _fab_completion run-fab
