d0bc137de7ea574032c18a4ca8e5c421ed8b1cbc
Mike Lang <mikelang3000@gmail.com>
2018-10-17 10:53:37 -0700

    ps1 alerts: display number, not on/off
    and make the alert listing command nicer

--- alerts.ps1.bash
+++ alerts.ps1.bash
@@ -1,6 +1,6 @@
 
 # Prefixes current PS1 with a blank space
-# This blank space changes to a ! character if any command
+# This blank space changes to a number if any command
 # given in the ALERTS array exits non-zero.
 # (additionally, a ? character is an error state - this shouldn't happen)
 # Tip: ALERTS may be appended to thusly: ALERTS+=('mycommand myarg')
@@ -12,22 +12,33 @@
 declare -a ALERTS[0]
 
 _alerts_check() {
+	local failed=0
 	for command in "${ALERTS[@]}"; do
 		if ! $command; then
-			echo -en '\b!'
-			return 1
+			failed=$((failed+1))
 		fi
 	done
-	echo -en '\b '
-	return 0
+	if [ "$failed" -gt 0 ]; then
+		echo -en "\b$failed"
+	else
+		echo -en '\b '
+	fi
+	return "$failed"
 }
 
 alerts() {
+	local verbose="$1"
 	for command in "${ALERTS[@]}"; do
-		echo "Alert: $command"
-		$command
-		echo "Exit status: $?"
-		echo
+		if [ -n "$verbose" ]; then
+			echo "Alert: $command"
+			$command
+			echo "Exit status: $?"
+			echo
+		else
+			if ! $command; then
+				echo "$command"
+			fi
+		fi
 	done
 }
 
