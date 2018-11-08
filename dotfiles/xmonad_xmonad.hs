704062241aae6b250e901d82846a05b1ae1b7165
Mike Lang <mikelang3000@gmail.com>
2018-11-08 06:25:25 -0800

    xmonad.hs: Replace default layout with custom one that can grow/shrink by the pixel
    This lets me exactly position it at the size I want, at the cost of taking ages to move

--- dotfiles/xmonad_xmonad.hs
+++ dotfiles/xmonad_xmonad.hs
@@ -1,13 +1,18 @@
 import XMonad
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Hooks.EwmhDesktops
- 
+import XMonad.Layout(Tall,Mirror,Full)
+
+-- Replace normal Tall layout with one that can grow/shrink by the pixel
+fineTall = Tall 1 (1/1920) (1/2)
+
 main = do
 	xmonad $ ewmh defaultConfig
 		{
 			terminal = "terminator",
 			modMask = myModMask,
-			handleEventHook = fullscreenEventHook
+			handleEventHook = fullscreenEventHook,
+			layoutHook = fineTall ||| Mirror fineTall ||| Full
 		}`additionalKeys`[
 		((myModMask, xK_p), spawn "dmenu_run"),
 		((myModMask, xK_e), spawn "dmenu-unicode")
