b04b353790045afb4aeb68342634111c10e7d2f9
Mike Lang <mikelang3000@gmail.com>
2019-07-18 19:37:50 -0700

    xmonad.hs: Add three extra workspaces on 0, -, =

--- dotfiles/xmonad_xmonad.hs
+++ dotfiles/xmonad_xmonad.hs
@@ -2,9 +2,13 @@ import XMonad
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout(Tall,Mirror,Full)
+import qualified XMonad.StackSet as W
 
 -- Replace normal Tall layout with one that can grow/shrink by the pixel
 fineTall = Tall 1 (1/1920) (1/2)
+
+-- three extra workspaces on keys 0, -, =
+extraWorkspaces = [(xK_0, "10"),(xK_minus, "11"),(xK_equal, "12")]
 
 main = do
 	xmonad $ ewmh defaultConfig
@@ -12,10 +16,23 @@ main = do
 			terminal = "terminator",
 			modMask = myModMask,
 			handleEventHook = fullscreenEventHook,
-			layoutHook = fineTall ||| Mirror fineTall ||| Full
-		}`additionalKeys`[
-		((myModMask, xK_p), spawn "dmenu_run"),
-		((myModMask, xK_e), spawn "dmenu-unicode")
-		]
+			layoutHook = fineTall ||| Mirror fineTall ||| Full,
+			-- three extra workspaces
+			workspaces = map show [1 .. 12]
+		} `additionalKeys` (myKeys)
+
+myKeys =
+	[
+	((myModMask, xK_p), spawn "dmenu_run"),
+	((myModMask, xK_e), spawn "dmenu-unicode")
+	] ++ [
+		-- switch to extra workspaces
+		((myModMask, key), (windows $ W.greedyView ws))
+		| (key,ws) <- extraWorkspaces
+	] ++ [
+		-- move window to extra workspaces
+		((myModMask .|. shiftMask, key), (windows $ W.shift ws))
+		| (key,ws) <- extraWorkspaces
+	]
 
 myModMask = mod4Mask
