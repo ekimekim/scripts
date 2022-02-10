4577b92e70db2444db5b88412ad57e5cd214527d
Mike Lang <mikelang3000@gmail.com>
2022-02-11 08:52:09 +1100

    use ecdsa key, not rsa

--- setup-keychain.bash
+++ setup-keychain.bash
@@ -3,7 +3,7 @@
 # You SHOULD pass in -Q unless you want an explicit check that all keys really are present
 
 KEYS=(
-	id_rsa
+	id_ecdsa
 )
 
 eval $(keychain --eval --agents ssh --quiet "$@" "${KEYS[@]}")
